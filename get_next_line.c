6ab02c73a462d414cbb75cb8a7800ba53fe3a2dd
sebastianpenning <whimizera@mailbox.org>
2023-12-05 22:11:04 +0100

    first draft get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -1,7 +1,32 @@
-
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 
 char *  get_next_line(int fd)
 {
+    ssize_t rtr;
+    char rtr_buf[11]; 
 
+    rtr = read(fd, rtr_buf, 9);
+    rtr_buf[11] = '\0';
+
+    printf("%ld\n", rtr);
+    printf("%s\n", rtr_buf);
+
+    return("lol");
+}
+
+
+int main ()
+{
+    unsigned int fd;
+
+    fd = open("./lol.txt", O_RDONLY);
+
+    get_next_line(fd);
+
+    printf("%d\n", fd);
+
+    close(fd);
 }
