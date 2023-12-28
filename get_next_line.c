b3931cafa48d01d7e11adb61fc737b037a61aeff
spenning <whimizera@mailbox.org>
2023-12-28 19:11:26 +0100

    working functionality with reading whole file

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,5 @@
 #include "get_next_line.h"
+#include <string.h>
 
 char	*ft_strdup(const char *s)
 {
@@ -90,7 +91,7 @@ int check_nextline(char *stat_buf)
     index=0;
     while(stat_buf[index] != '\0')
     {
-		printf("check_nl: %c\n", stat_buf[index]);
+		// printf("check_nl: %c\n", stat_buf[index]);
         if (stat_buf[index] == '\n')
         {
             return(index);
@@ -105,14 +106,14 @@ char * realloc_statbuf(char **stat_buf, size_t gnl, size_t stat_buf_len)
 	char * temp;
 
 	temp = *stat_buf;
-	*stat_buf = ft_substr(*stat_buf, gnl, stat_buf_len - gnl);
+	*stat_buf = ft_substr(*stat_buf, gnl + 1, stat_buf_len - gnl);
 	free(temp);
 	return(*stat_buf);
 }
 
 int read_buffer(int fd, char** stat_buf)
 {
-    ssize_t rtr;
+    static ssize_t rtr;
 	char read_buf[BUFFER_SIZE + 1];
 	char * temp;
 
@@ -126,6 +127,11 @@ int read_buffer(int fd, char** stat_buf)
             read_buf[rtr] = '\0';
         else
             read_buf[BUFFER_SIZE] = '\0';
+		// printf("read buffer stat_buf: %s\n", *stat_buf);
+		if(!*stat_buf && rtr == 0)
+			return(0);
+		if(!*stat_buf)
+			*stat_buf = ft_strdup("");
 		temp = *stat_buf;
 		*stat_buf = strjoin(*stat_buf, read_buf);
 		free(temp);
@@ -136,59 +142,69 @@ int read_buffer(int fd, char** stat_buf)
 char * return_str(char **stat_buf, size_t gnl)
 {
 	char * ret;
+	int 	strlcpy_int;
 	if(gnl == 0 && !*stat_buf)
 		return(NULL);
 	ret = malloc(gnl + 2);
 	if(ret == NULL)
 		return(NULL);
-	ft_strlcpy(ret, *stat_buf, gnl + 1);
+	strlcpy_int = ft_strlcpy(ret, *stat_buf, gnl + 2);
+	if(strlcpy_int == 0)
+	{
+		free(ret);
+		return(NULL);
+	}
 	return(ret);
 }
 
 char *  get_next_line(int fd)
 {
     static char 	*stat_buf;
-	// char 			*temp;
 	char 			*ret;
 	size_t			gnl;
 	size_t			stat_buf_len;
 
 	if(fd == 0 || BUFFER_SIZE == 0)
 		return(NULL);
-
-	if(!stat_buf)
-		stat_buf = ft_strdup("");
 	if(read_buffer(fd, &stat_buf) == 0)
+	{
+		free(stat_buf);
 		return(NULL);
+	}
 	stat_buf_len = ft_strlen(stat_buf);
 	gnl = check_nextline(stat_buf);
-	printf("stat_buf_len= %ld\ngnl= %ld\n", stat_buf_len, gnl);
+	// printf("stat_buf_len= %ld\ngnl= %ld\n", stat_buf_len, gnl);
+	// printf("stat_buf= %s\n", stat_buf);
 	ret = return_str(&stat_buf, gnl);
-	if(gnl < stat_buf_len)
+	// printf("ret= %d\n", ret[0]);
+	if(gnl < stat_buf_len && ret != NULL)
 		realloc_statbuf(&stat_buf, gnl, stat_buf_len);
-	printf("stat_buf= %s\n", stat_buf);
-	// ret = malloc(gnl + 1);
-	// printf("statbuf: %ld\n gnl: %ld\n %s\n %s\n", stat_buf_len, gnl, stat_buf, ret, stat_buf);
-    return(ret);
+	if(gnl == stat_buf_len)
+	{	
+		// printf("stat_buf: %s\n", stat_buf);
+		free(stat_buf);
+		stat_buf = NULL;
+		// printf("stat_buf: %s\n", stat_buf);
+	}
+	return(ret);
 }
 
 
-int main ()
-{
-    unsigned int fd;
-	char *test;
-    fd = open("./test.txt", O_RDONLY);
+// int main ()
+// {
+//     unsigned int fd;
+// 	char *test;
+//     fd = open("./test.txt", O_RDONLY);
 
-	test = get_next_line(fd);
-	printf("return: \n%s\n", test);
-	test = get_next_line(fd);
-	printf("return: \n%s\n", test);
-	test = get_next_line(fd);
-	printf("return: \n%s\n", test);
-	free(test); 
+// 	test = get_next_line(fd);
+// 	printf("return: \n%s\n", test);
+// 	free(test); 
+// 	test = get_next_line(fd);
+// 	printf("return: \n%s\n", test);
+// 	free(test);
 
-    close(fd);
-}
+//     close(fd);
+// }
 
 
 
