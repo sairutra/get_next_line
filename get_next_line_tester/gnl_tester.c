d1835ed7ee6a18a9856c50df07d98b3bb8712883
sebastianpenning <whimizera@mailbox.org>
2024-01-11 17:18:29 +0100

    added new format of output of tester + correct testing null cases

--- get_next_line_tester/gnl_tester.c
+++ get_next_line_tester/gnl_tester.c
@@ -2,6 +2,18 @@
 #include <fcntl.h>
 #include "../get_next_line.h" 
 
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[1;32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define BMAG   "\x1B[1;35m"
+#define CYN   "\x1B[36m"
+#define BCYN   "\x1B[1;36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+
 static size_t	test_strlen(const char *s)
 {
 	size_t	index;
@@ -42,20 +54,24 @@ static int	test_strncmp(const char *s1, const char *s2, size_t n)
 }
 
 
-int tester (const char * path, char *str, int testnbr, int linenbr)
+int tester (const int fd, char *str, int testnbr, int linenbr)
 {
 	int 	rtr;
-	int		fd;
 	char 	*gnl;
 	int		error_index;
 	FILE 	*errorLog;
 
 	error_index = 0; 
 	rtr = 0;
-	fd = open(path, O_RDONLY);
 	gnl = get_next_line(fd);
-	rtr += test_strncmp(str, gnl, test_strlen(str));
-	if (rtr != 0)
+	if(str && gnl)
+	{
+		if(test_strlen(gnl) > test_strlen(str))
+			rtr += test_strncmp(str, gnl, test_strlen(gnl));
+		else
+			rtr += test_strncmp(str, gnl, test_strlen(str));
+	}
+	if (rtr != 0 || (gnl && !str) || (!gnl && str))
 	{
 		errorLog = fopen("logs/error_log.txt", "a");
 
@@ -65,27 +81,60 @@ int tester (const char * path, char *str, int testnbr, int linenbr)
         	printf("Error opening the file");
         	return 1;
     	}
-		printf("Test %d\n", testnbr);
-		printf("FAIL\n");
-		printf("Line number: %d\n", linenbr);
-		printf("Check error_log.txt\n");
+		printf(RED "%d " RESET, testnbr);
+		printf(RED "FAIL " RESET);
+		printf(RED "Check error_log.txt\n" RESET);
 		fprintf(errorLog, "----------------\n");
 		fprintf(errorLog, "Test number %d\n", testnbr);
 		fprintf(errorLog, "Line %d\n", linenbr);
-		while (str[error_index] != '\0')
-			fprintf(errorLog, "expected output: %d\n", str[error_index++]);
+		if(str)
+		{
+			while (str[error_index] != '\0')
+				fprintf(errorLog, "expected output: %d\n", str[error_index++]);
+		}
 		error_index = 0;
 		fprintf(errorLog, "expected output: %s\n", str);
-		while (gnl[error_index]!= '\0')
-			fprintf(errorLog, "gnl output: %d\n", gnl[error_index++]);
+		if(gnl)
+		{
+			while (gnl[error_index]!= '\0')
+				fprintf(errorLog, "gnl output: %d\n", gnl[error_index++]);
+		}
 		fprintf(errorLog, "gnl output: %s\n", gnl);
 		fprintf(errorLog, "strncmp difference: %d\n", rtr);
 		fclose(errorLog);
 		free(gnl);
 		return(0);
 	}
-	printf("Test %d OK\n", testnbr);
+	printf(GRN "%d OK " RESET, testnbr );
 	free(gnl);
+	return(0);
+}
+
+
+void welcome_screen()
+{
+	printf(CYN "<==================================================================================>\n" RESET);
+	printf(CYN "||                                                                                ||\n" RESET);
+	printf(CYN "||    ||      |      ||  ||||||  ||     |||||   ||||||    |||| |||||   ||||||     ||\n" RESET);
+	printf(CYN "||    ||     |||     ||  ||      ||    ||      ||    ||  ||   ||   ||  ||         ||\n" RESET);
+	printf(CYN "||    ||    || ||    ||  ||||||  ||    ||      ||    ||  ||   ||   ||  ||||||     ||\n" RESET);
+	printf(CYN "||     ||  ||   ||  ||   ||      ||    ||      ||    ||  ||   ||   ||  ||         ||\n" RESET);
+	printf(CYN "||      ||||     ||||    ||||||  |||||  |||||   ||||||   ||   ||   ||  ||||||     ||\n" RESET);
+	printf(CYN "||                                                                                ||\n" RESET);
+	printf(CYN "<==================================================================================>\n" RESET);
+	printf(CYN "+                                                                                  +\n" RESET);
+	printf(CYN "+                             " RESET);
+	printf(YEL "get_next_line" RESET);
+	printf(CYN " tester" RESET);
+	printf(CYN "                                 +\n" RESET);
+	printf(CYN "+                                  by spenning                                     +\n" RESET);
+	printf(CYN "+                                                                                  +\n" RESET);
+	printf(CYN "<==================================================================================>\n" RESET);
+}
+
+int fail_print()
+{
+	printf(RED "Program crashed\n" RESET);
 	return(0);
 }
 
@@ -93,16 +142,37 @@ int main (void)
 {
 	int testnbr;
 	int linenbr;
-
-	testnbr = 0;
-	linenbr = 0;
+	int fd;
 	fclose(fopen("logs/error_log.txt", "w"));
-	printf("------------------\n");
-	printf("|     tests x    |\n");
-	printf("------------------\n");
-	testnbr++;
-	linenbr++;
-	if(tester("test_files/test.txt", "lololol\n", testnbr, linenbr) == 1)
-		return(0);
+	welcome_screen();
+
+	testnbr = 1;
+	linenbr = 1;
+	printf(BMAG "empty.txt:\n" RESET);
+	fd = open("test_files/empty.txt", O_RDONLY);
+	if(tester(fd, NULL, testnbr, linenbr) == 1)
+		return(fail_print());
+	if(tester(fd, NULL, ++testnbr, ++linenbr) == 1)
+		return(fail_print());
+	if(tester(fd, NULL, ++testnbr, ++linenbr) == 1)
+		return(fail_print());
+	close(fd);
+	printf("\n");
+
+	testnbr = 1;
+	linenbr = 1;
+	printf(BMAG "1char.txt:\n" RESET);
+	fd = open("test_files/1char.txt", O_RDONLY);
+	if(tester(fd, "1\n", testnbr, linenbr) == 1)
+		return(fail_print());
+	if(tester(fd, "1\n", ++testnbr, ++linenbr) == 1)
+		return(fail_print());
+	if(tester(fd, "1", ++testnbr, ++linenbr) == 1)
+		return(fail_print());
+	close(fd);
+	printf("\n");
+	linenbr = 0;
+
+
 	return(1);
 }
